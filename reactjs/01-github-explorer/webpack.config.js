07da3b6755a61b9668f77569b47a11e939313475
Ragner Alves <ragnerf@hotmail.com>
2021-04-21 17:50:23 -0300

    html-estatico

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -10,6 +11,11 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    plugins: [
+        new HtmlWebPackPlugin({
+            temnplate: path.resolve(__dirname, 'public', 'index.html')
+        })
+],
     module: {
         rules: [
             {
