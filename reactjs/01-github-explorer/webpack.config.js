717ea225dc87443cdd1e3a1723d7d4e619287917
Ragner Alves <ragnerf@hotmail.com>
2021-04-21 17:36:54 -0300

    estrutura-react

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,4 +19,4 @@ module.exports = {
             }            
         ],
     }
-};
+}
