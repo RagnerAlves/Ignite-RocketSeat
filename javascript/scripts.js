64e033e572719d57e00131274f1c7f01c85b0bff
Ragner Alves <ragnerf@hotmail.com>
2021-04-27 20:40:14 -0300

    tips_data

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,17 @@
-console.log('Bem vindo ao Starter !!')
+// console.log('Bem vindo ao Starter !!')
+
+// console.log({       //se é uma funcionalidade, então é um metodo pois o mesmo está dentro de um objeto.
+//     name: "Ragner",
+//     idade: 28,
+//     andar: function(){
+//         console.log('andar')
+//     }
+// })
+
+// console.log([           //array
+//     "Ovos",
+//     "Leite",
+//     "Pão",
+//     2,
+//     3
+// ])
