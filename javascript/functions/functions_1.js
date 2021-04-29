806f2968f049c841a930d936972ff7f912d36229
Ragner Alves <ragnerf@hotmail.com>
2021-04-29 17:32:46 -0300

    operators

--- javascript/functions/functions_1.js
+++ javascript/functions/functions_1.js
@@ -1,13 +1,13 @@
 
-//agrapamento, reutilização e dar significado
+//agrupamento, reutilização e dar significado
 //declaração da função
 //function statement
-function creatPhrases(){
+// function creatPhrases(){
 
-    console.log('Estudar é muito bom')
-    console.log('Paciencia e persistência')
-    console.log('Revisão é mãe do aprendizado')
-}
+//     console.log('Estudar é muito bom')
+//     console.log('Paciencia e persistência')
+//     console.log('Revisão é mãe do aprendizado')
+// }
 
 //executar function, reutilização de código
-creatPhrases()
+//creatPhrases()
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -90,3 +90,141 @@
 // let index = techs.indexOf('css')  //buscar qual elemento do array
 // techs.splice(index, 1) 
 // console.log(index)
+
+//Expressões e operadores
+
+// let number
+
+// (function() {
+//     console.log('alo')
+// })()
+
+//new, criar um novo objeto
+
+// let name = new String('Titio')
+// let age = new Number(28)
+// console.log(name, age)
+
+//operadora unarios
+
+// const person = {
+//     name:'Titio',
+//     age: 28,
+// }
+
+// delete person.age()
+// console.log(person )
+
+//multiplicação
+//console.log(1 * 5)
+
+//divisao
+//console.log(10 / 2)
+
+//soma
+//console.log( 5 + 5)
+
+//subtracao
+//console.log( 7-3 )
+
+//resto da divisao
+//let remainder =
+//remainder = 11 % 10
+//console.log(remainder)
+
+//incremento
+//let increment = 0
+//increment++
+//console.log(increment)
+
+//decremento
+//let decrement = 0
+//decrement--
+//console.log(increment)
+
+//exponencial
+//console.log( 3**3 )
+
+//operadores de comparação
+// let one = 1
+// let two = 2
+//igual a 
+// console.log( two == 1)
+// console.log( two == "1")
+//diferente de
+// console.log( one != two)
+// console.log( one != 1)
+// console.log( one != "1")
+
+//estritamente igual a 
+// console.log( one === "1")
+// console.log( one === 1)
+//estriatamente diferente de
+// console.log( two !== "2")
+// console.log( two !== 2)
+
+//maior que 
+//console.log( one > two)
+//maior igual
+// console.log( one >= 1)
+// console.log( two >= 1)
+//menor que
+//console.log( one < two)
+//menor igual
+// console.log( one <= two)
+// console.log( one <= 1)
+// console.log( one <= 0)
+
+//atribuicao
+//let x = 12
+//console.log(x)
+//additional 
+//x += 2
+//subtracao
+//x -= 1
+//multiplication
+//x *= 2
+//division
+//x /= 2
+//resto
+//x %= 10
+//exponencial
+//x **= 5
+
+//operadores logicos
+
+// let pao = true
+// let queijo = true
+
+//and
+//console.log(pao && queijo)
+//or
+//console.log( pao || queijo)
+//not
+//console.log(!pao)
+
+//cafe da manha top termanrio
+
+// let pao = true
+// let queijo = true
+
+// const niceBreakfast = pao && queijo ?'Café top' : 'Café ruim'
+// console.log(niceBreakfast)
+
+//mairo de 18 anos
+
+// let age = 16
+// const canDrive = age >= 18 ? 'can drive' : 'not  drive'
+// console.log(canDrive)
+
+//Operador de String
+
+//console.log('a' === 'a')
+
+//concatenacao
+//console.log('a' + 'a')
+
+//type conversion vs type coersion
+//FALSY
+
+console.log(true ? 'verdadeiro' : 'false')
