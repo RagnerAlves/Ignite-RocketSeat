51b560060f72174c50ca55801483ac9d4c027902
Ragner Alves <ragnerf@hotmail.com>
2021-04-30 17:11:11 -0300

    update

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -227,4 +227,45 @@
 //type conversion vs type coersion
 //FALSY
 
-console.log(true ? 'verdadeiro' : 'false')
+//console.log(true ? 'verdadeiro' : 'false')
+
+// switch (expressio){
+//     case 'a':
+//         //        
+//         break;
+//         case 'a':        
+// }
+
+//estrutura de repeticao FOR
+
+// for(let i = 0; i < 20; i++ ){
+//     console.log(i)
+// }
+
+
+// Exercicio escola
+
+// let nota = 90
+// let notaA = nota >= 90 && nota <= 100
+// let notaB = nota >= 80 && nota <= 89
+// let notaC = nota >= 70 && nota <= 79
+// let notaD = nota >= 60 && nota <= 69
+// let notaF = nota < 60 && nota >= 0
+
+// let notaFinal;
+
+// if(notaA){
+//     notaFinal = 'A'
+// } else if(notaB){
+//     notaFinal = 'B'
+// } else if (notaC) {
+//     notaFinal = 'C'
+// } else if (notaD) {
+//     notaFinal = 'D'
+// } else if (notaF){
+//     notaFinal = 'F'
+// } else {
+//     console.log('Nota invalida')
+// }
+
+// console.log(notaFinal)
