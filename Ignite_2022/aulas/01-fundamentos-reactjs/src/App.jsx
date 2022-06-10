7dfd58e3f3a669409403197db524961b770f3fb2
Ragner Alves <ragnerf@hotmail.com>
2022-06-10 14:46:43 -0300

    Estrutura_App_react

---
+++ Ignite_2022/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Ragner Alves" 
+        content="Text text text tex"
+      />
+      <Post 
+        author="Titio Alves" 
+        content="Text21 text21 text21 tex12"
+      />
+    </div>
+  )
+}
