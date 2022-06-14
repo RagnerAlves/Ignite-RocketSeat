aaa780ef9366a9fef89cea2d0d9b41defc63d54c
Ragner Alves <ragnerf@hotmail.com>
2022-06-14 15:06:09 -0300

    Estrut_App_Css_Modules

--- Ignite_2022/aulas/01-fundamentos-reactjs/src/App.jsx
+++ Ignite_2022/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import {Header} from './components/Header';
 import {Post} from './Post'
+import './style.css'
 
 export function App() {
   return (
   <div>
+
+    <Header />
+
     <Post 
       author="Ragner ALves"
       content="Dev"
