0160ba570b7178225e8bd425e7ac169532b609cf
Ragner Alves <ragnerf@hotmail.com>
2022-06-27 10:23:59 -0300

    Update_sidebar

--- Ignite_2022/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ Ignite_2022/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -6,15 +8,18 @@ export function Sidebar() {
             <img 
 
                 className={styles.cover} 
-                src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
+                src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt=""
+            />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/RagnerAlves.png"></img>
                 <strong> Ragner Alves.</strong>
                 <span> Developer </span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil.
                 </a>
             </footer>
